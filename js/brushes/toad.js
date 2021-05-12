8a6c57726e968219f59030e42f7862a2e0091a3b
Jane Tao <78860345+jtao7@users.noreply.github.com>
2021-05-12 17:15:44 +0900

    Revert "Merge from class repo"

--- js/brushes/toad.js
+++
@@ -1,9 +0,0 @@
-/* global mouseIsPressed, image, mouseX, mouseY */
-window.brushes.toad = {
-  name: 'toad',
-  draw: function () {
-    if (mouseIsPressed) {
-      image(toad, mouseX, mouseY, 50, 50);
-    }
-  }
-}
